bca6ace47259a236b386c2f54261824b2cd654d6
kaylbie <technologyhero2014@gmail.com>
2024-04-17 21:31:09 +0300

    Network

--- Sabotage Express/Assets/!/Scripts/Network/NetworkManagerCustom.cs
+++ Sabotage Express/Assets/!/Scripts/Network/NetworkManagerCustom.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 
 public class NetworkManagerCustom : NetworkBehaviour
@@ -11,6 +12,9 @@ public class NetworkManagerCustom : NetworkBehaviour
 
     private void Awake()
     {
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(
+            "192.168.1.66", (ushort)7777, "0.0.0.0"
+        );
         if (Instance == null)
         {
             Instance = this;
